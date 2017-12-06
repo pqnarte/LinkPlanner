d508103eb8f0d441fb5a0f73094f0f18d3cd714e
Mariana <marianaferreiraramos@ua.pt>
2017-12-06 11:37:51 +0000

    6/12/2017 Mariana Before Meeting

--- include/optical_switch.h
+++ include/optical_switch.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef OPTICAL_SWITCH_H_
+#define OPTICAL_SWITCH_H_
+
+#include "netxpto.h"
+
+class OpticalSwitch : public Block {
+
+public:
+	OpticalSwitch(vector <Signal*> &inputSignals, vector <Signal*> &outputSignals) : Block(inputSignals, outputSignals) {};
+
+	void initialize(void);
+
+	bool runBlock(void);
+};
+#endif // !OPTICAL_SWITCH_H_
