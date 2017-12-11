8cbddd8c41e4acc29feffae82c3b4caccdde929e
Mariana <marianaferreiraramos@ua.pt>
2017-12-11 14:39:36 +0000

    before merge 11/12

--- include/alice_qkd.h
+++ include/alice_qkd.h
@@ -2,10 +2,38 @@
 #define ALICE_QKD_H
 
 #include "netxpto.h"
+#include "binary_source.h"
+#include "m_qam_mapper.h"
+#include "discrete_to_continuous_time.h"
+#include "pulse_shaper.h"
+#include "sink.h"
 
+class AliceQKD : public SuperBlock {
 
-class AliceQKD : public Block {
-	
+	/* State Variables */
+
+	// #####################################################################################################
+	// ################## Internal Signals Declaration and Inicialization ##################################
+	// #####################################################################################################
+	Binary S1{ "NUM_A.sgn" };
+	TimeDiscreteAmplitudeDiscreteReal S2{ "MQAM_I.sgn" };
+	TimeDiscreteAmplitudeDiscreteReal S3{ "MQAM_Q.sgn" };
+	TimeContinuousAmplitudeDiscreteReal S4{ "S4.sgn" };
+	TimeContinuousAmplitudeContinuousReal S5{ "S5.sgn" };
+
+	// #####################################################################################################
+	// ########################### Blocks Declaration and Inicialization ###################################
+	// #####################################################################################################
+	BinarySource B1;
+
+	MQamMapper B2;
+
+	DiscreteToContinuousTime B3;
+
+	PulseShaper B4;
+
+	Sink B5;
+
 public:
 	/* input parameters*/
 
@@ -13,11 +41,28 @@ public:
 	int StringPhotonsLength{ 12 };
 
 	// Methods
-	AliceQKD (vector <Signal*> &inputSignals, vector <Signal*> &outputSignals) : Block(inputSignals, outputSignals) {};
+	AliceQKD (vector <Signal*> &inputSignals, vector <Signal*> &outputSignals);
 
-	void initialize(void);
+	// Set methods
+	void set(int opt);
 
-	bool runBlock(void);
+	void setMode(BinarySourceMode m) { B1.setMode(m); };
+	BinarySourceMode const getMode(void) { return B1.getMode(); };
+
+	void setProbabilityOfZero(double pZero) { B1.setProbabilityOfZero(pZero); };
+	double const getProbabilityOfZero(void) { return B1.getProbabilityOfZero(); };
+
+	void setBitStream(string bStream) { B1.setBitStream(bStream); };
+	string const getBitStream(void) { return B1.getBitStream(); };
+
+	void setNumberOfBits(long int nOfBits) { B1.setNumberOfBits(nOfBits); }
+	long int const getNumberOfBits(void) { return B1.getNumberOfBits(); }
+
+	void setPatternLength(int pLength) { B1.setPatternLength(pLength); }
+	int const getPatternLength(void) { return B1.getPatternLength(); }
+
+	void setBitPeriod(double bPeriod) { B1.setBitPeriod(bPeriod); };
+	double const getBitPeriod(void) { return B1.getBitPeriod(); }
 
 	void setRateOfPhotons(double RPhotons) { RateOfPhotons = RPhotons; };
 	double const getRateOfPhotons(void) { return RateOfPhotons; };
@@ -25,6 +70,22 @@ public:
 	void setStringPhotonsLength(int pLength) { StringPhotonsLength = pLength; };
 	int const getStringPhotonsLength(void) { return StringPhotonsLength; };
 
+	void setM(int mValue) { B2.m = mValue; };
+	int const getM(void) { return B2.m; };
+
+	void setIqAmplitudes(vector<t_iqValues> iqAmplitudesValues) { B2.m = iqAmplitudesValues.size(); B2.iqAmplitudes.resize(B1.m); B2.iqAmplitudes = iqAmplitudesValues; };
+	vector<t_iqValues> const getIqAmplitudes(void) { return B2.iqAmplitudes; };
+
+	void setNumberOfSamplesPerSymbol(int n) { B3.setNumberOfSamplesPerSymbol(n); };
+	int const getNumberOfSamplesPerSymbol(void) { return B3.getNumberOfSamplesPerSymbol(); };
+
+	void setRollOffFactor(double rOffFactor) { B5.setRollOffFactor(rOffFactor); };
+	double const getRollOffFactor(void) { return B5.getRollOffFactor(); };
+
+	void setSeeBeginningOfImpulseResponse(bool sBeginningOfImpulseResponse) { B5.setSeeBeginningOfImpulseResponse(sBeginningOfImpulseResponse); };
+	double const getSeeBeginningOfImpulseResponse(void) { return B5.getSeeBeginningOfImpulseResponse(); };
+
+	void setPulseShaperFilter(PulseShaperFilter fType) { B5.setFilterType(fType); };
 
 };
 
--- lib/alice_qkd.cpp
+++ lib/alice_qkd.cpp
@@ -1,11 +1,42 @@
 #include "netxpto.h"
 #include "alice_qkd.h"
 
-void AliceQKD::initialize(void) {
+AliceQKD::AliceQKD(vector <Signal*> &inputSignal, vector<Signal *> &outputSignal) :SuperBlock(inputSignal, outputSignal) {
+	numberOfInputSignals = inputSignal.size();
+	numberOfOutputSignals = outputSignal.size();
 
+	signal_value_type sType = outputSignals[0]->getValueType();
+
+	switch (numberOfOutputSignals) {
+	case 1:
+		B1.initializeBlock(vector<Signal*> {}, vector<Signal*> { &S1});
+		B2.initializeBlock(vector<Signal*> { &S1 }, vector<Signal*> { &S2, &S3 });
+		B3.initializeBlock(vector<Signal*> { &S2 }, vector<Signal*> { &S4 });
+		B4.initializeBlock(vector<Signal*> { &S4 }, vector<Signal*> { &S5 });
+		B5.initializeBlock(vector<Signal*> { &S4 }, vector<Signal*> {});
+		
+		setModuleBlocks({ &B1, &B2, &B3, &B4, &B5 });
+		break;
+	default:
+		cout << "ERRO: alice_qkd.cpp" << "\n";
+	}
 }
 
-bool AliceQKD::runBlock(void) {
+void AliceQKD::set(int opt) {
 
-	return 0;
+	// Basic Configuration
+
+	if (opt == 0) {
+		setMode(DeterministicAppendZeros);
+		setBitPeriod(1.0 / 50e9);
+		setPatternLength(5);
+		setNumberOfBits(10000);
+		setNumberOfSamplesPerSymbol(16); //antes 32
+		setRollOffFactor(0.3);
+		setIqAmplitudes({ { -45, 0 },{ 0, 0 },{ 45, -0 },{ 90, 0 } });
+		setOutputOpticalPower_dBm(0); //antes 0
+		setSaveInternalSignals(true);
+	}
+
+	return;
 }
