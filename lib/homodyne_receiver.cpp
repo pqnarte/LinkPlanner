2c9f200ac23db2ce3af860a03b9f281b68683be7
Andoni Santos <5340263+andonisantos@users.noreply.github.com>
2018-01-05 17:21:46 +0000

    Changed structure of homodyne_receiver.cpp blocks to match the corresponding header file.

--- lib/homodyne_receiver.cpp
+++ lib/homodyne_receiver.cpp
@@ -7,6 +7,7 @@ HomodyneReceiver::HomodyneReceiver(vector<Signal *> &inputSignal, vector<Signal 
 	inputSignals = inputSignal;
 	outputSignals = outputSignal;
 
+/*
 	B1.initializeBlock(vector<Signal*> { }, vector<Signal*> { &HMD01 }); //Local oscillator
 	B2.initializeBlock(vector<Signal*> { inputSignals[0], &HMD01 }, vector<Signal*> { &HMD02, &HMD03, &HMD04, &HMD05 }); //Optical hybrid
 	B3.initializeBlock(vector<Signal*> { &HMD02, &HMD03 }, vector<Signal*> { &HMD06 }); //Photodiodes
@@ -19,7 +20,25 @@ HomodyneReceiver::HomodyneReceiver(vector<Signal *> &inputSignal, vector<Signal 
 	B10.initializeBlock(vector<Signal*> { &HMD10 }, vector<Signal*> { &HMD13 }); //Sampler
 	B11.initializeBlock(vector<Signal*> { &HMD11 }, vector<Signal*> { &HMD14 }); //Sampler
 	B12.initializeBlock(vector<Signal*> { &HMD13, &HMD14 }, vector<Signal*> { &HMD15 }); //Decoder
+	*/
 
-	setModuleBlocks({ &B1, &B2, &B3, &B4, &B5, &B6, &B7, &B8, &B9, &B10, &B11, &B12 });
+	B1.initializeBlock(vector<Signal*> { }, vector<Signal*> { &HMD01 }); //Local oscillator
+	B2.initializeBlock(vector<Signal*> { inputSignals[0], &HMD01 }, vector<Signal*> { &HMD02, &HMD03, &HMD04, &HMD05 }); //Optical hybrid
+	B3.initializeBlock(vector<Signal*> { &HMD02, &HMD03 }, vector<Signal*> { &HMD06 }); //Photodiodes
+	B4.initializeBlock(vector<Signal*> { &HMD04, &HMD05 }, vector<Signal*> { &HMD07 }); //Photodiodes
+	B5.initializeBlock(vector<Signal*> { &HMD06 }, vector<Signal*> { &HMD08 }); //Amplifier
+	B6.initializeBlock(vector<Signal*> { &HMD07 }, vector<Signal*> { &HMD09 }); //Amplifier
+	B7.initializeBlock(vector<Signal*> { }, vector<Signal*> { &HMD10 }); //Noise
+	B8.initializeBlock(vector<Signal*> { }, vector<Signal*> { &HMD11 }); //Noise
+	B9.initializeBlock(vector<Signal*> { &HMD08, &HMD10 }, vector<Signal*> { &HMD12 }); //Add
+	B10.initializeBlock(vector<Signal*> { &HMD09, &HMD11 }, vector<Signal*> { &HMD13 }); //Add
+	B11.initializeBlock(vector<Signal*> { &HMD12 }, vector<Signal*> { &HMD14 }); //Pulse Shaper
+	B12.initializeBlock(vector<Signal*> { &HMD13 }, vector<Signal*> { &HMD15 }); //Pulse Shaper
+	B13.initializeBlock(vector<Signal*> { &HMD14 }, vector<Signal*> { &HMD16 }); //Sampler
+	B14.initializeBlock(vector<Signal*> { &HMD15 }, vector<Signal*> { &HMD17 }); //Sampler
+	B15.initializeBlock(vector<Signal*> { &HMD16, &HMD17 }, vector<Signal*> { &HMD18 }); //Decoder
+
+//	setModuleBlocks({ &B1, &B2, &B3, &B4, &B5, &B6, &B7, &B8, &B9, &B10, &B11, &B12 });
+	setModuleBlocks({ &B1, &B2, &B3, &B4, &B5, &B6, &B7, &B8, &B9, &B10, &B11, &B12, &B13, &B14, &B15 });
 	
 };
