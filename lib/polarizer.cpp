42d5ac3414282a34d31b4a936d85b7607becf343
NetXPTO <netxpto@gmail.com>
2018-01-08 13:54:10 +0000

    2018-01-08

--- lib/polarizer.cpp
+++ lib/polarizer.cpp
@@ -1,14 +1,15 @@
 #include "netxpto.h"
 #include "polarizer.h"
 
+
 void Polarizer::initialize(void) {
 	outputSignals[0]->setFirstValueToBeSaved(inputSignals[1]->getFirstValueToBeSaved());
 	outputSignals[0]->setSamplingPeriod(inputSignals[1]->getSamplingPeriod());
-	outputSignals[0]->setFirstValueToBeSaved(inputSignals[1]->getFirstValueToBeSaved());
 	outputSignals[0]->setSamplesPerSymbol(inputSignals[1]->getSamplesPerSymbol());
 }
 
 bool Polarizer::runBlock(void) {
+	
 	int ready1 = inputSignals[0]->ready();
 	int ready2 = inputSignals[1]->ready();
 
@@ -19,7 +20,6 @@ bool Polarizer::runBlock(void) {
 	int length = min(ready, space);
 
 	if (length <= 0) return false;
-
 
 	t_complex_xy valueXY;
 	t_complex valueX, valueY, valueXout, ValueYout;
@@ -39,8 +39,7 @@ bool Polarizer::runBlock(void) {
 		valueXY = { valueXout, ValueYout };
 
 		outputSignals[0]->bufferPut(valueXY);
-
 	}
 
-	return 0;
+	return true;
 }
