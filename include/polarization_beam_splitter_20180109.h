292cc5bc8ba3f0aa0edfcc5df3aaaf409afb8f3e
NetXPTO <netxpto@gmail.com>
2018-01-31 22:25:38 +0000

    trivial

--- include/polarization_beam_splitter_20180109.h
+++ include/polarization_beam_splitter_20180109.h
@@ -1,47 +1,36 @@
 # ifndef POLARIZATION_BEAM_SPLITTER_H_
 # define POLARIZATION_BEAM_SPLITTER_H_
 
-<<<<<<< HEAD
-=======
-#include "netxpto_20180118.h"
-
+#include "netxpto.h"
 #include <array>
->>>>>>> Develop.Mariana_TQ
 
 class PolarizationBeamSplitter : public Block {
+	/* State variables */
+	bool firstTime{ true };
 
 	/* input parameters */
 
-<<<<<<< HEAD
-=======
 	t_real F = 1;
 	t_complex unit = 1;
-	array <t_complex, 4> matrixH = { { F*unit, 0, 0, 0 } };
-	array <t_complex, 4> matrixV = { { 0, 0, 0, F*unit } };
+	array <t_complex, 4> matrix = { { F*unit, 0, 0, F*unit } };
 	/*
 	Jones Matrix for PBS:
-	1	0
-	0	1
+
+		1	0
+		0	1
 	*/
 
->>>>>>> Develop.Mariana_TQ
 public:
 
-	PolarizationBeamSplitter(vector<Signal *> &inputSignals, vector<Signal *> &outputSignals) : Block(inputSignals, outputSignals) {};
+	PolarizationBeamSplitter(vector<Signal *> &inputSignals, vector<Signal *> &outputSignals) : Block(inputSignals, outputSignals){};
 
 	void initialize(void);
 
 	bool runBlock(void);
 
-<<<<<<< HEAD
-=======
-	void setTransferMatrixH(array<complex<double>, 4> TransferMatrix) { matrixH = TransferMatrix; }
-	array<complex<double>, 4> const getTransferMatrixH(void) { return matrixH; }
+	void setTransferMatrix(array<complex<double>, 4> TransferMatrix) { matrix = TransferMatrix; }
+	array<complex<double>, 4> const getTransferMatrix(void) { return matrix; }
 
-	void setTransferMatrixV(array<complex<double>, 4> TransferMatrix) { matrixV = TransferMatrix; }
-	array<complex<double>, 4> const getTransferMatrixV(void) { return matrixV; }
-
->>>>>>> Develop.Mariana_TQ
 };
 
 #endif
