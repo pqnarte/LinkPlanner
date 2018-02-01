018777e503e4d0d41a3200aa22ee24bdceada233
Andoni Santos <5340263+andonisantos@users.noreply.github.com>
2018-02-01 15:55:46 +0000

    Started working on proper seeding for white noise generator.

--- lib/white_noise_20180118.cpp
+++ lib/white_noise_20180118.cpp
@@ -10,8 +10,69 @@
 
 void WhiteNoise::initialize(void){
 
-	firsTime = false;
+	firstTime = false;
+	/*
+	switch (getSeedType()) {
 
+		case RandomDevice:
+		{
+			generate_n(seed_data.data(), seed_data.size(), randomDevice());
+			seed_seq seq1(begin(seed_data), end(seed_data));
+			generator1.seed(seq1);
+
+			generate_n(seed_data.data(), seed_data.size(), randomDevice());
+			seed_seq seq2(begin(seed_data), end(seed_data));
+			generator2.seed(seq2);
+
+			generate_n(seed_data.data(), seed_data.size(), randomDevice());
+			seed_seq seq3(begin(seed_data), end(seed_data));
+			generator3.seed(seq3);
+
+			generate_n(seed_data.data(), seed_data.size(), randomDevice());
+			seed_seq seq4(begin(seed_data), end(seed_data));
+			generator4.seed(seq4);
+
+			break;
+		}
+		case DeterministicDefault:
+		{
+			// temporary default seed values
+			// the generator needs to be seeded even by default
+			// otherwise noise may end up being correlated
+
+			seed_seq seq1{ 17171717 };
+			generator1.seed(seq1);
+
+			seed_seq seq2{ 27315883 };
+			generator2.seed(seq2);
+
+			seed_seq seq3{ 4387621 };
+			generator3.seed(seq3);
+
+			seed_seq seq4{ 91827631 };
+			generator4.seed(seq4);
+
+			break;
+		}
+		case DeterministicSelected:
+		{
+			// temporary implementation for user determined seeding
+
+			seed_seq seq1{seed};
+			generator1.seed(seq1); 
+
+			seed_seq seq2{seed+1};
+			generator2.seed(seq2);
+
+			seed_seq seq3{seed+2};
+			generator3.seed(seq3);
+
+			seed_seq seq4{seed+3};
+			generator4.seed(seq4);
+			break;
+		}
+};
+	*/
 //	outputSignals[0]->setSamplingPeriod(samplingPeriod);
 //	outputSignals[0]->setSymbolPeriod(symbolPeriod);
 
