07102d43ebb00a6f11d110b7d7c4c3dd4a37479d
andremourato <andremourato@ua.pt>
2018-02-05 22:06:59 +0000

    Improved VHDL simulation module. Created separate package

--- vhdl_simulation/vhdl_simulation.vhd
+++ vhdl_simulation/vhdl_simulation.vhd
@@ -4,8 +4,10 @@ use IEEE.STD_LOGIC_TEXTIO.all;
 use IEEE.NUMERIC_STD.all;
 use STD.TEXTIO.all;
 
+use work.SignalProcessing.all;
+
 entity vhdl_simulation is
-	generic(FILENAME : string := "S5.sgn");
+	generic(FILENAME : string := "SIGNAL.sgn");
 end vhdl_simulation;
 
 architecture Behavioral of vhdl_simulation is
@@ -27,31 +29,30 @@ begin
 	
 io_proc: process
 				
-				-- The simulation must be executed in the directory \vhdl_simulation\
-				constant file_input_path : string := ".\input_files\" & FILENAME;
-				constant file_output_path : string := ".\output_files\" & FILENAME;
-				variable char_buffer  : myType; -- bits received
-				variable vector_buffer : std_logic_vector(7 downto 0);
-				variable new_line_count : integer := 0; -- Counts the 4 newLine characters to ignore the HEADER
+				variable char_buffer_in    : myType; -- character received
+				variable char_buffer_out   : myType; -- character ouputted
+				variable vector_buffer_in  : std_logic_vector(7 downto 0);
+				variable vector_buffer_out : std_logic_vector(7 downto 0);
+				variable new_line_count    : integer := 0; -- Counts the 4 newLine characters to ignore the HEADER
 				
 			begin
-			
-				file_open(file_input_signal, file_input_path, READ_MODE);
-				file_open(file_output_signal, file_output_path, WRITE_MODE);
+				-- The simulation must be executed in the directory \vhdl_simulation\
+				file_open(file_input_signal, ".\input_files\" & FILENAME, READ_MODE);
+				file_open(file_output_signal, ".\output_files\" & FILENAME, WRITE_MODE);
 				
 				while not endfile(file_input_signal) loop -- while there is information to be read
 					-------------------------------------------
 					-- Character Read
 					-------------------------------------------
 					-- Reads the input bits from the input file
-					read(file_input_signal, char_buffer);
+					read(file_input_signal, char_buffer_in);
 					-------------------------------------------
 					-- HEADER Remover
 					-------------------------------------------
 					-- If /= 4 it's still reading the HEADER
 					if(new_line_count /= 4) then
 						-- If the current char is a new line character
-						if(character'pos(char_buffer) = 10) then
+						if(character'pos(char_buffer_in) = 10) then
 						-- if new_line_count = 4, means HEADER is over
 							new_line_count :=  new_line_count + 1;
 						end if;
@@ -60,14 +61,14 @@ io_proc: process
 					-------------------------------------------
 					-- Computation
 					-------------------------------------------
-					vector_buffer := std_logic_vector(to_unsigned(character'pos(char_buffer),8));
-					-- computation(vector_buffer); -- Signal Processing
-					char_buffer := character'val(to_integer(unsigned(vector_buffer)));
+					vector_buffer_in := std_logic_vector(to_unsigned(character'pos(char_buffer_in),8));
+					computation(vector_buffer_in,vector_buffer_out); -- Signal Processing
+					char_buffer_out := character'val(to_integer(unsigned(vector_buffer_out)));
 					-------------------------------------------
 					-- Write back
 					-------------------------------------------
 					-- Writes the output bits to the output file
-					write(file_output_signal, char_buffer);
+					write(file_output_signal, char_buffer_out);
 				end loop;
 				
 				file_close(file_input_signal);
