750eb0cc14b5dff8a63dcc4407b78165354e02e6
Mariana_TQ <marianaferreiraramos@ua.pt>
2018-02-07 15:58:59 +0000

    Mariana 20180207

--- lib/message_interface_20180205.cpp
+++ lib/message_interface_20180205.cpp
@@ -18,114 +18,62 @@ void MessageProcessorAlice::initialize(void) {
 bool MessageProcessorAlice::runBlock(void) {
 
 	/*Number of message ready to be read*/
-	int ready = inputSignals[1]->ready();
+	int readyIn = inputSignals[1]->ready();
 
 	int spaceIn = spaceBufferIn();					//in units of bits
-	spaceIn = spaceIn / messageDataLength;		//in units of messages
+	spaceIn = (int) floor(spaceIn / messageDataLength);		//in units of messages
 
-	int process = min(ready, spaceIn);
+	int process = min(readyIn, spaceIn);
 
-	for (auto k = 1; k <= process; k++) {
-		t_message message;
-		inputSignals[1]->bufferGet(&message);
+	if (spaceIn <= 0) {
+		bufferFullIn = true;
+	}
+	if (inPositionBufferIn == 0) {
+		bufferEmptyIn = true;
 	}
 
-	/*Number of bits ready to be read*/
-	int ready2 = inputSignals[0]->ready();
+	if (!bufferFullIn) {
+		for (auto k = 0; k < process; k++) {
+			t_message message;
+			inputSignals[1]->bufferGet(&message);
+			t_message_field messageField = message.at(k);
 
-	/*State variable set*/
-	StateMachine = IdleState;
+			string messageFieldName = messageField.fieldName;
+			string messageFieldValue = messageField.fieldValue;
 
-	bool alive = false;
-
-	/*While there are bits to process from binary source*/
-/*	while (ready2 != 0) {
-		if (ready1 != 0) {
-			int mType;
-			int emptySpace;
-
-			int space1 = outputSignals[0]->space();
-			int space2 = outputSignals[1]->space();
-
-			int length = min(space1, space2);
-
-			if (length <= 0) return false;
-
-			switch (StateMachine) {
-				case enableToWrite:
-					int auxIn;
-					for (int i = 0; i < dataLength; i++) {
-						inputSignals[1]->bufferGet(&auxIn);
-						pBufferIn[i] = auxIn;
-						bIn++;
-					}
-					newMessage = true;
-					StateMachine = idleState;
-					break;
-
-				case enableToRead:
-					for (bOut = 0; bOut <= bufferLength; bOut++) {
-						pBufferOut[bOut] = pBufferIn[bOut];
-					}
-					StateMachine = bitsToProcess;
-					break;
-
-				case bitsToProcess:
-					int auxBitsOut;
-
-					while (bIn != 0)
-					{
-						outputSignals[1]->bufferPut(mType);
-						outputSignals[1]->bufferPut(dataLength);
-
-						for (bOut = 0; bOut <= dataLength; bOut++) {
-							inputSignals[0]->bufferGet(&auxBitsOut);
-							if (pBufferOut[bOut] == 0 || pBufferOut[bOut] == 1) {
-								if (pBufferOut[bOut] == auxBitsOut) {
-									outputSignals[0]->bufferPut(pBufferOut[bOut]);
-									outputSignals[1]->bufferPut(pBufferOut[bOut]);
-								}
-							}
-							else {
-								outputSignals[1]->bufferPut(pBufferOut[bOut]);
-							}
-							bIn--;
-						}
-					}
-					StateMachine = idleState;
-					break;
-
-				case idleState:
-					emptySpace = bufferLength - bIn;
-					if (newMessage == true)
-					{
-						inputSignals[1]->bufferGet(&mType);
-						inputSignals[1]->bufferGet(&dataLength);
-						newMessage = false;
-
-						if (mType == basisReconciliation) {
-							if (emptySpace >= dataLength) {
-								StateMachine = enableToWrite;
-							}
-							else {
-								if (bIn == bufferLength) {
-									StateMachine = enableToRead;
-								}
-							}
-
-						}
-
-					}
-					else
-						alive = true;
-
-					break;
+			for (unsigned l = 0; l < messageFieldValue.length(); l++) {
+				pBufferIn[inPositionBufferIn] = (int)messageFieldValue.at(l);
+				inPositionBufferIn++;
 			}
 		}
+		bufferEmptyIn = false;
 	}
-	
-*/	
-	return alive;
+
+	/*Message available to be processed*/
+	int readyIn2 = inputSignals[0]->ready();
+
+	int spaceOut = spaceBufferOut();
+	spaceOut = (int)floor(spaceOut / messageDataLength);
+	int processOut = min(readyIn2, spaceOut);
+
+	if (spaceOut <= 0) {
+		bufferFullOut = true;
+	}
+
+	if (outPositionBufferOut == 0) {
+		bufferEmptyOut = true;
+	}
+
+	if ((processOut > 0) && (!bufferFullOut)) {
+		for (auto m = 0; m < messageDataLength; m++) {
+			pBufferOut[outPositionBufferOut] = pBufferIn[inPositionBufferOut];
+			outPositionBufferOut++;
+			inPositionBufferIn--;
+		}
+		bufferEmptyOut = false;
+	}
+
+	return true;
 }
 
 int MessageProcessorAlice::spaceBufferIn() {
