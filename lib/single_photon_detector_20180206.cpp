504026e2ce4d30df4d07199a64afaf25c7cea14e
Mariana <marianaferreiraramos@ua.pt>
2018-02-13 20:26:23 +0000

    20180212 Mariana_TQ

--- lib/single_photon_detector_20180206.cpp
+++ lib/single_photon_detector_20180206.cpp
@@ -1,12 +1,9 @@
 # include "single_photon_detector_20180206.h"
 
-SinglePhotonDetector::SinglePhotonDetector(vector<Signal *> &inputSignals, vector<Signal *> &outputSignals) :Block(inputSignals, outputSignals) {
+void SinglePhotonDetector::initialize(void) {
+
 	numberOfInputSignals = (int)inputSignals.size();
 	numberOfOutputSignals = (int)outputSignals.size();
-}
-
-
-void SinglePhotonDetector::initialize(void) {
 
 	for (auto k = 0; k < numberOfOutputSignals; k++) {
 		outputSignals[k]->setSymbolPeriod(inputSignals[0]->getSymbolPeriod());
@@ -25,18 +22,13 @@ bool SinglePhotonDetector::runBlock(void) {
 	}
 	else {
 		int ready;
-		switch (numberOfInputSignals)
-		{
-		case 1:
+		if (numberOfInputSignals == 1) {
 			ready = inputSignals[0]->ready();
-			break;
-		case 2:
+		}
+		else if (numberOfInputSignals == 2) {
 			int ready1 = inputSignals[0]->ready();
 			int ready2 = inputSignals[1]->ready();
 			ready = min(ready1, ready2);
-			break;
-		default:
-			break;
 		}
 
 		int space = outputSignals[0]->space();
@@ -51,114 +43,65 @@ bool SinglePhotonDetector::runBlock(void) {
 		generator.seed(seed);
 
 		signal_value_type inSignalType = inputSignals[0]->getValueType();
-		switch (inSignalType) {
-		case PhotonValue:
-			for (auto k = 0; k < process; k++) {
-				t_photon inValue;
-				inputSignals[0]->bufferGet(&inValue);
-				if (inValue.probabilityAmplitude == 1) {
-					outputSignals[0]->bufferPut((t_real) 1.0);
-				}
-				else {
-					outputSignals[0]->bufferPut((t_real) 0.0);
-				}
-			}
-			break;
-		case PhotonValueMP:
-			for (auto k = 0; k < process; k++) {
-				t_photon_mp inValueMP;
-				inputSignals[0]->bufferGet(&inValueMP);
-				if (inValueMP.path[path].probabilityAmplitude > 0.0) {	// Process Photon Path
+
+		for (auto k = 0; k < process; k++) {
+			t_real clk;
+			if (numberOfOutputSignals > 1) 
+				inputSignals[1]->bufferGet(&clk);
+			else 
+				clk = 1.0;
+			
+
+			t_photon_mp_xy inValueMP;
+			inputSignals[0]->bufferGet(&inValueMP);
+
+			t_complex_xy inValue = (t_complex_xy)inValueMP.path[path];
+			t_complex xValue = inValue.x;
+			t_complex yValue = inValue.y;
+
+			switch (path) {
+			case 0:
+				if ((abs(xValue) >= 0.0) && (abs(xValue) <= 1.0) && (clk > 0.0)) {
 					double number = distribution(generator);
-					if (number < pow(inValueMP.path[path].probabilityAmplitude, 2)) {
-						outputSignals[0]->bufferPut((t_real) 1.0);
-						inValueMP.path[(path + 1) % 2].probabilityAmplitude = 0.0;
-						inValueMP.path[path].probabilityAmplitude = -1.0; // Photon Path Processed
+					if (number < (pow(abs(xValue), 2) + probabilityDarkCount)) {
+						outputSignals[0]->bufferPut((t_real)1.0);
+						inValueMP.path[1].x = (t_complex) 0.0;
+						inValueMP.path[1].y = (t_complex) 0.0;
 					}
 					else {
-						outputSignals[0]->bufferPut((t_real) 0.0);
-						inValueMP.path[(path + 1) % 2].probabilityAmplitude = 1.0;
-						inValueMP.path[path].probabilityAmplitude = -1.0; // Photon Path Processed
+						outputSignals[0]->bufferPut((t_real)0.0);
+						inValueMP.path[1].x = (t_complex) 0.0;
+						inValueMP.path[1].y = (t_complex) 1.0;
 					}
-				};
-				if (inValueMP.path[path].probabilityAmplitude == 0.0) {
-					outputSignals[0]->bufferPut((t_real) 0.0);
-					inValueMP.path[path].probabilityAmplitude = -1.0; // Photon Path Processed
 				}
-				if (inValueMP.path[(path + 1) % 2].probabilityAmplitude >= 0.0) {
-					inputSignals[0]->bufferPut((t_photon_mp)inValueMP);
-				}
-			}
-			break;
 
-		case PhotonValueMPXY:
-			for (auto k = 0; k < process; k++) {
-				t_real clk;
-				inputSignals[1]->bufferGet(&clk);
+				break;
 
-				if (clk > 0) {
-					t_photon_mp_xy inValueMP;
-					inputSignals[0]->bufferGet(&inValueMP);
-
-					t_complex_xy inValue = (t_complex_xy)inValueMP.path[path];
-					t_complex xValue = inValue.x;
-					t_complex yValue = inValue.y;
-
-
-
-					switch (path) {
-					case 0:
-						if ((abs(xValue) >= 0.0) && (abs(xValue) <= 1.0)) {
-							double number = distribution(generator);
-							if (number < (pow(abs(xValue), 2) + probabilityDarkCount)) {
-								outputSignals[0]->bufferPut((t_real)1.0);
-								inValueMP.path[1].x = (t_complex) 0.0;
-								inValueMP.path[1].y = (t_complex) 0.0;
-							}
-							else {
-								outputSignals[0]->bufferPut((t_real)0.0);
-								inValueMP.path[1].x = (t_complex) 0.0;
-								inValueMP.path[1].y = (t_complex) 1.0;
-							}
-						}
-
-						break;
-
-					case 1:
-						if (abs(yValue) == 1.0)
+			case 1:
+				if (clk > 0.0){
+					if (abs(yValue) == 1.0)
+						outputSignals[0]->bufferPut((t_real)1.0);
+					else {
+						double number = distribution(generator);
+						if (number < probabilityDarkCount) {
 							outputSignals[0]->bufferPut((t_real)1.0);
-						else {
-							double number = distribution(generator);
-							if (number < probabilityDarkCount) {
-								outputSignals[0]->bufferPut((t_real)1.0);
-							}
-							else
-								outputSignals[0]->bufferPut((t_real)0.0);
 						}
-
-						break;
-
-					default:
-						cout << "x:" << abs(xValue) << "y:" << abs(yValue) << "\n";
-						break;
-
-					}
-
-					if ((abs(inValueMP.path[1].y) == 0.0) || (abs(inValueMP.path[1].y) == 1.0)) {
-						inputSignals[0]->bufferPut((t_photon_mp_xy)inValueMP);
+						else
+							outputSignals[0]->bufferPut((t_real)0.0);
 					}
 				}
-				else {
+				break;
 
-				}
+			default:
+				cout << "x:" << abs(xValue) << "y:" << abs(yValue) << "\n";
+				break;
 
 			}
 
-			break;
-		default:
-			cout << "ERRO: single_photon_detector.cpp" << "\n";
-			return false;
-		};
+			if ((abs(inValueMP.path[1].y) == 0.0) || (abs(inValueMP.path[1].y) == 1.0) && (clk > 0.0)) {
+				inputSignals[0]->bufferPut((t_photon_mp_xy)inValueMP);
+			}
+		}
 
 	}
 
