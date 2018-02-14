d07549d21cca583f7f69d782aff4dbce88f8c0c7
Mariana_TQ <marianaferreiraramos@ua.pt>
2018-02-14 14:59:41 +0000

    mariana

--- lib/bobBB84_20180206.cpp
+++ lib/bobBB84_20180206.cpp
@@ -9,7 +9,7 @@ void bobBB84::initialize(void) {
 
 	outputSignals[1]->setSymbolPeriod(inputSignals[1]->getSymbolPeriod());
 	outputSignals[1]->setSamplingPeriod(inputSignals[1]->getSamplingPeriod());
-	outputSignals[1]->setSamplesPerSymbol(1);
+	outputSignals[1]->setSamplesPerSymbol(inputSignals[1]->getSamplesPerSymbol());
 	outputSignals[1]->setFirstValueToBeSaved(inputSignals[1]->getFirstValueToBeSaved());
 
 }
@@ -19,7 +19,6 @@ bool bobBB84::runBlock(void) {
 	int ready0 = inputSignals[0]->ready();
 	int ready1 = inputSignals[1]->ready();
 	int ready = min(ready0, ready1);
-	ready = min(ready, messageDataLength);
 
 	int space0 = outputSignals[0]->space();
 	int space1 = outputSignals[1]->space();
@@ -29,7 +28,6 @@ bool bobBB84::runBlock(void) {
 
 	if (process < 0) return false;
 
-	string messageDataOut;
 	for (auto k = 0; k < process; k++) {
 		t_binary basisIn;
 		inputSignals[0]->bufferGet(&basisIn);
@@ -37,17 +35,15 @@ bool bobBB84::runBlock(void) {
 		inputSignals[1]->bufferGet(&measurement);
 
 		if ((measurement == 0.0) || (measurement == 1.0)) {
-			messageDataOut.append(to_string(basisIn));
-			outputSignals[0]->bufferPut((t_binary)basisIn);
+			outputSignals[0]->bufferPut((t_real)basisIn);
+			outputSignals[1]->bufferPut((t_real)basisIn);
 		}
 		else {
-			messageDataOut.append(to_string(measurement));
-			//discard -1 and -2
+			outputSignals[0]->bufferPut((t_real)measurement);
+			outputSignals[1]->bufferPut((t_real)measurement);
 		}
+		
 	}
-	t_message messageOut;
-	setMessageData(messageOut, messageDataOut);
-	setMessageDataLength(messageOut, to_string(process));
 
 	return true;
 }
