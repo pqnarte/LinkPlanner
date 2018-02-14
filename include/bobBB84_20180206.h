dc508eb591bcdcbcf75ab87f1b451125e15d7d92
Mariana_TQ <marianaferreiraramos@ua.pt>
2018-02-14 11:29:24 +0000

    mariana

--- include/bobBB84_20180206.h
+++ include/bobBB84_20180206.h
@@ -3,9 +3,14 @@
 
 #include "netxpto_20180118.h"
 
+typedef int t_message_data_length;
+typedef string t_message_data;
+typedef enum { BasisReconciliation } t_message_type;
+
 class bobBB84 : public Block {
 
 	/*Input Parameters*/
+	int messageDataLength{ 64 };
 
 public:
 
@@ -15,6 +20,13 @@ public:
 	void initialize(void);
 
 	bool runBlock(void);
+
+	void setMessageDataLength(t_message msg, t_message_data_length mDLength) { msg.messageDataLength = to_string(mDLength); };
+
+	void setMessageData(t_message msg, t_message_data mData) { msg.messageData = mData; };
+
+	void setMessageType(t_message msg, t_message_type mType) { msg.messageType = mType; };
+
 
 };
 
--- lib/bobBB84_20180206.cpp
+++ lib/bobBB84_20180206.cpp
@@ -2,11 +2,52 @@
 
 void bobBB84::initialize(void) {
 
+	outputSignals[0]->setSymbolPeriod(inputSignals[0]->getSymbolPeriod());
+	outputSignals[0]->setSamplingPeriod(inputSignals[0]->getSamplingPeriod());
+	outputSignals[0]->setSamplesPerSymbol(inputSignals[0]->getSamplesPerSymbol());
+	outputSignals[0]->setFirstValueToBeSaved(inputSignals[0]->getFirstValueToBeSaved());
+
+	outputSignals[1]->setSymbolPeriod(inputSignals[1]->getSymbolPeriod());
+	outputSignals[1]->setSamplingPeriod(inputSignals[1]->getSamplingPeriod());
+	outputSignals[1]->setSamplesPerSymbol(1);
+	outputSignals[1]->setFirstValueToBeSaved(inputSignals[1]->getFirstValueToBeSaved());
 
 }
 
 bool bobBB84::runBlock(void) {
 	
+	int ready0 = inputSignals[0]->ready();
+	int ready1 = inputSignals[1]->ready();
+	int ready = min(ready0, ready1);
+	ready = min(ready, messageDataLength);
+
+	int space0 = outputSignals[0]->space();
+	int space1 = outputSignals[1]->space();
+	int space = min(space0, space1);
+
+	int process = min(ready, space);
+
+	if (process < 0) return false;
+
+	string messageDataOut;
+	for (auto k = 0; k < process; k++) {
+		t_binary basisIn;
+		inputSignals[0]->bufferGet(&basisIn);
+		t_real measurement;
+		inputSignals[1]->bufferGet(&measurement);
+
+		if ((measurement == 0.0) || (measurement == 1.0)) {
+			messageDataOut.append(to_string(basisIn));
+			outputSignals[0]->bufferPut((t_binary)basisIn);
+		}
+		else {
+			messageDataOut.append(to_string(measurement));
+			//discard -1 and -2
+		}
+	}
+	t_message messageOut;
+	setMessageData(messageOut, messageDataOut);
+	setMessageDataLength(messageOut, to_string(process));
 
 	return true;
 }
