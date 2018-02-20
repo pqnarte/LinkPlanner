0b6dc322a1eed3a4837c1dcecaf4499ddd7e8ff0
Mariana_TQ <marianaferreiraramos@ua.pt>
2018-02-20 16:55:56 +0000

    Mariana

--- lib/alice_qkd.cpp
+++ lib/alice_qkd.cpp
@@ -10,18 +10,20 @@ AliceQKD::AliceQKD(vector<Signal*> &inputSignal, vector <Signal*> &outputSignal)
 	BA1.initializeBlock(vector<Signal*>{inputSignals[1]}, vector<Signal*>{&alice_1, &alice_2});
 	BA2.initializeBlock(vector<Signal*>{&alice_1}, vector<Signal*>{&alice_3});
 	BA4.initializeBlock(vector<Signal*>{&alice_2}, vector<Signal*>{});
-	BA3.initializeBlock(vector<Signal*>{&alice_3}, vector<Signal*>{&S4});
+	BA4.setNumberOfSamples(1000);
+	BA4.setDisplayNumberOfSamples(true);
+	BA3.initializeBlock(vector<Signal*>{&alice_3}, vector<Signal*>{&alice_4});
 	BA11.initializeBlock(vector<Signal*>{}, vector<Signal*>{&alice_8});
 	BA11.setMode(DeterministicCyclic);
 	BA11.setBitStream("01");
-	BA6.initializeBlock(vector<Signal*>{inputSignals[2], &alice_8}, vector<Signal*>{&S5, &alice_7});
+	BA6.initializeBlock(vector<Signal*>{inputSignals[2], &alice_8}, vector<Signal*>{&alice_6, &alice_7});
 	BA12.initializeBlock(vector<Signal*>{&alice_7}, vector<Signal*>{});
 	BA13.initializeBlock(vector<Signal*>{}, vector<Signal*>{&alice_11});
 	BA13.setMode(DeterministicCyclic);
 	BA13.setBitStream("10");
-	BA8.initializeBlock(vector<Signal*>{inputSignals[3],&alice_11}, vector<Signal*>{&S8,&alice_10});
+	BA8.initializeBlock(vector<Signal*>{inputSignals[3],&alice_11}, vector<Signal*>{&alice_9,&alice_10});
 	BA14.initializeBlock(vector<Signal*>{&alice_10}, vector<Signal*>{});
-	BA5.initializeBlock(vector<Signal*>{inputSignals[0], &S4, &S5, &S8}, vector<Signal*>{outputSignals[0], outputSignals[1], outputSignals[2], outputSignals[3]});
+	BA5.initializeBlock(vector<Signal*>{inputSignals[0], &alice_4, &alice_6, &alice_9}, vector<Signal*>{&clkA_out, &alice_4_out, &alice_6_out,&alice_9_out});
 
 	setModuleBlocks({ &BA1 , &BA2, &BA4, &BA3 , &BA11, &BA6, &BA12, &BA13, &BA8, &BA14, &BA5});
 
--- lib/bob_qkd.cpp
+++ lib/bob_qkd.cpp
@@ -5,30 +5,23 @@ BobQKD::BobQKD(vector<Signal*> &inputSignal, vector <Signal*> &outputSignal) : S
 	BB0.initializeBlock(vector <Signal*> {inputSignals[0]}, vector<Signal*>{&Bob_1});
 
 	BB1.initializeBlock(vector <Signal*> {&Bob_1}, vector<Signal*>{&Bob_2});
+	Bob_2.setSamplingPeriod((1 / 1e6) / 16);
+	Bob_2.setSymbolPeriod(1 / 1e6);
+
 	BB2.initializeBlock(vector <Signal*> {&Bob_2}, vector<Signal*>{&Bob_8});
 
 	BB3.initializeBlock(vector <Signal*> {inputSignals[1]}, vector<Signal*>{&Bob_3});
 	BB4.initializeBlock(vector <Signal*> {&Bob_3}, vector<Signal*>{&CLKB_out1, &CLKB_out2});
 
-	BB10.initializeBlock(vector <Signal*> {inputSignals[2], inputSignals[3]}, vector<Signal*>{&Bob_7});
+	BB10.initializeBlock(vector<Signal*>{inputSignals[2], inputSignals[3]}, vector<Signal*>{&Bob_7});
 
-	BB5.initializeBlock(vector <Signal*> {&Bob_7}, vector<Signal*>{&Bob_4, &Bob_10});
-
-	BBSink.initializeBlock(vector<Signal*>{&Bob_4}, vector<Signal*>{});
-	BBSink.setNumberOfSamples(1000);
+	BBSink.initializeBlock(vector<Signal*>{&Bob_7}, vector<Signal*>{});
 	BBSink.setDisplayNumberOfSamples(true);
 
-	BBSink2.initializeBlock(vector<Signal*>{&Bob_10}, vector<Signal*>{});
-	BBSink2.setDisplayNumberOfSamples(true);
-	BBSink2.setNumberOfSamples(1000);
-	
-	BBSink1.initializeBlock(vector<Signal*>{inputSignals[4]}, vector<Signal*>{});
-	BBSink1.setNumberOfSamples(1000);
-	BBSink1.setDisplayNumberOfSamples(true);
 
-	BB9.initializeBlock(vector <Signal*> {&Bob_8, &CLKB_out1, &CLKB_out2}, vector<Signal*>{&S4, outputSignals[1], outputSignals[2]});
+	BB9.initializeBlock(vector <Signal*> {&Bob_8, &CLKB_out1, &CLKB_out2}, vector<Signal*>{outputSignals[0], outputSignals[1], outputSignals[2]});
 
-	setModuleBlocks({ &BB0, &BB1, &BB2, &BB3, &BB4, &BB10, &BB5, &BBSink, &BBSink2,&BBSink1, &BB9 });
+	setModuleBlocks({ &BB0, &BB1, &BB2, &BB3, &BB4, &BB10, &BBSink, &BB9 });
 
 
 }
