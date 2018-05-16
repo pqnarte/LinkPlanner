b6069d5d31b1933fa46240aec31e66b7f049d892
andremourato <andremourato@ua.pt>
2018-05-16 19:32:23 +0000

    Update BPSK layout

--- sdf/bpsk_system/bpsk_system_sdf.cpp
+++ sdf/bpsk_system/bpsk_system_sdf.cpp
@@ -44,14 +44,14 @@ public:
 	}
 
 	/* Initializes input parameters according to the program arguments*/
-	BPSKInputParameters(int argc, char*argv[]) : SystemInputParameters(argc,argv) {
+	BPSKInputParameters(int argc, char*argv[]) : SystemInputParameters(argc, argv) {
 		initializeInputParameterMap(); //Initializes the parameters
 		readSystemInputParameters();
 	}
 
 	//METHODS
 	//Each parameter must be added to the parameter map by calling addInputParameter(string,param*)
-	void initializeInputParameterMap(){
+	void initializeInputParameterMap() {
 		addInputParameter("numberOfBitsReceived", &numberOfBitsReceived); //Cria parametro numberOfBitsReceived
 		addInputParameter("numberOfBitsGenerated", &numberOfBitsGenerated); //Cria parametro numberOfBitsGenerated
 		addInputParameter("samplesPerSymbol", &samplesPerSymbol); //Cria parametro samplesPerSymbol
@@ -79,41 +79,32 @@ int main(int argc, char *argv[]) {
 	// ########################### Signals Declaration and Inicialization ##################################
 	// #####################################################################################################
 
-	Binary S0("S0.sgn");
+	Binary S0("S0.sgn", param.getOutputFolderName());
 	S0.setBufferLength(param.bufferLength);
-	S0.setFolderName(param.getOutputFolderName());
 
-	OpticalSignal S1("S1.sgn");
+	OpticalSignal S1("S1.sgn", param.getOutputFolderName());
 	S1.setBufferLength(param.bufferLength);
-	S1.setFolderName(param.getOutputFolderName());
 
-	OpticalSignal S2("S2.sgn");
+	OpticalSignal S2("S2.sgn", param.getOutputFolderName());
 	S2.setBufferLength(param.bufferLength);
-	S2.setFolderName(param.getOutputFolderName());
 
-	OpticalSignal S3("S3.sgn");
+	OpticalSignal S3("S3.sgn", param.getOutputFolderName());
 	S3.setBufferLength(param.bufferLength);
-	S3.setFolderName(param.getOutputFolderName());
 
-	OpticalSignal S4("S4.sgn");
+	OpticalSignal S4("S4.sgn", param.getOutputFolderName());
 	S4.setBufferLength(param.bufferLength);
-	S4.setFolderName(param.getOutputFolderName());
 
-	TimeContinuousAmplitudeContinuousReal S5("S5.sgn");
+	TimeContinuousAmplitudeContinuousReal S5("S5.sgn", param.getOutputFolderName());
 	S5.setBufferLength(param.bufferLength);
-	S5.setFolderName(param.getOutputFolderName());
 
-	TimeDiscreteAmplitudeContinuousReal S6("S6.sgn");
+	TimeDiscreteAmplitudeContinuousReal S6("S6.sgn", param.getOutputFolderName());
 	S6.setBufferLength(param.bufferLength);
-	S6.setFolderName(param.getOutputFolderName());
 
-	Binary S7("S7.sgn");
+	Binary S7("S7.sgn", param.getOutputFolderName());
 	S7.setBufferLength(param.bufferLength);
-	S7.setFolderName(param.getOutputFolderName());
 
-	Binary S8("S8.sgn");
+	Binary S8("S8.sgn", param.getOutputFolderName());
 	S8.setBufferLength(param.bufferLength);
-	S8.setFolderName(param.getOutputFolderName());
 
 	// #####################################################################################################
 	// ########################### Blocks Declaration and Inicialization ###################################
@@ -151,7 +142,7 @@ int main(int argc, char *argv[]) {
 
 	Sampler B5{ vector<Signal*> {&S5}, vector<Signal*> {&S6} };
 	B5.setSamplesToSkip(param.samplesToSkip);
-	
+
 	BitDecider B6{ vector<Signal*> {&S6}, vector<Signal*> {&S7} };
 
 	BitErrorRate B7{ vector<Signal*> {&S0, &S7}, vector<Signal*> {&S8} };
@@ -165,14 +156,14 @@ int main(int argc, char *argv[]) {
 	// ########################### System Declaration and Inicialization ###################################
 	// #####################################################################################################
 
-	System MainSystem{ vector<Block*> { &B1, &B2, &B3, &B4, &B5, &B6, &B7, &B8} };
-	MainSystem.setSignalsFolderName(param.getOutputFolderName());
-	MainSystem.setLoadedInputParameters(param.getLoadedInputParameters());
+	System MainSystem{ vector<Block*> { &B1, &B2, &B3, &B4, &B5, &B6, &B7, &B8},
+						param.getOutputFolderName(),
+						param.getLoadedInputParameters()};
 
 	// #####################################################################################################
 	// #################################### System Run #####################################################
 	// #####################################################################################################
-	
+
 	MainSystem.run();
 
 	return 0;
